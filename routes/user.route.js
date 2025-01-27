dd9a87c219d05f2d71a3c66429568f0c137897e5
HaR-S-H <h8551283@gmail.com>
2025-01-27 23:22:13 +0530

    week 3 backend done

---
+++ routes/user.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { login, logout, register } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/mutler.js";
+ 
+const router = express.Router();
+
+router.route("/register").post(singleUpload,register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+
+export default router;
